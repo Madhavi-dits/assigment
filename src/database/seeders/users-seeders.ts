5cb83015deeb8446c3f8daebf815af74c9015d29
Madhavi Sharma <madhavi.sharma@ditstek.com>
2023-08-10 13:19:53 +0530

    updated seeder

--- src/database/seeders/users-seeders.ts
+++ src/database/seeders/users-seeders.ts
@@ -6,16 +6,16 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('Users', [
       {
-        firstName: 'Madhavi',
-        lastName: 'Sharma',
-        email: 'madhavi@yopmail.com',
+        firstName: 'Super',
+        lastName: 'Admin',
+        email: 'superadmin@yopmail.com',
         gender:'Female',
         dob:'07-08-1998',
         address:'chandigarh',
         createdAt: new Date(),
         updatedAt: new Date(),
         role:'superadmin',
-        phoneNumber: '9691629030',
+        phoneNumber: '7428730894',
         password: password,
         passwordUpdateAt: new Date(),
         isVerified: 1
